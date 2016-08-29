b48caceee9e14c8a38e7bb4a3e46c4feff0d3a44
unknown <AskewR04@UNL102476.uniad.unitec.ac.nz>
2016-08-29 17:26:00 +1200

    init commit

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP_Alt.Startup))]
+namespace ASP_Alt
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
